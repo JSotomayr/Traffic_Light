f708e26ed332f4718525bf9e624291b568682805
Jaime Sotomayor <89789808+JSotomayr@users.noreply.github.com>
2021-11-18 14:24:53 +0100

    Update trafficLight.jsx

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -1,1 +1,23 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+
+const Bulb = props => {
+	return (
+		<Fragment>
+			<div
+				className={props.light}
+				onClick={() => {
+					props.switchLight(props.light);
+				}}
+			/>
+		</Fragment>
+	);
+};
+
+Bulb.propTypes = {
+	switchLight: PropTypes.func,
+	light: PropTypes.string
+};
+
+export default Bulb;
 
