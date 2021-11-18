3aefdf63b3113d923eac3ebe9994f1190ed21917
Jaime Sotomayor <89789808+JSotomayr@users.noreply.github.com>
2021-11-18 14:24:30 +0100

    Update home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,1 +1,52 @@
+import React, { useState } from "react";
+import Bulb from "./bulb.jsx";
 
+const Home = () => {
+	const [color, setColor] = useState({
+		stop: "empty",
+		ready: "empty",
+		run: "empty"
+	});
+
+	return (
+		<div>
+			
+
+				<div className="pole-bar"></div>
+			<div className="container">
+				<Bulb
+					light={color.stop}
+					switchLight={() => {
+						setColor({
+							stop: "red",
+							ready: "empty",
+							run: "empty"
+						});
+					}}
+				/>
+				<Bulb
+					light={color.ready}
+					switchLight={() => {
+						setColor({
+							stop: "empty",
+							ready: "yellow",
+							run: "empty"
+						});
+					}}
+				/>
+				<Bulb
+					light={color.run}
+					switchLight={() => {
+						setColor({
+							stop: "empty",
+							ready: "empty",
+							run: "green"
+						});
+					}}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
